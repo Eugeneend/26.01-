f090509b7a01a956eb3f28c8ea2b9b236e75f0d5
Eugene <Oleksienko2003@gmail.com>
2023-01-26 20:27:33 +0200

    26.01 класс

---
+++ app.js
@@ -0,0 +1,116 @@
+'use strict'
+
+// 1 завдання
+
+// let film = 'Мій улюблений фільм '
+
+// let tip =' Ironman'
+
+// console.log(film + tip);
+
+// console.log(`film "${tip}"`)
+
+// 2 задание
+
+// let userName = 'Добрий день'
+
+// let Name = prompt("Введіть своє ім'я" , 'Хтось')
+
+// console.log(`${userName}  ${Name}`);
+
+// 3 задание
+
+// let pas = prompt('Введіть пароль !')
+
+// let rez 
+
+// if (pas == "Step" || pas == 'Web' || pas ==  'JavaScript') { rez = 'Підтвердженно';
+    
+// }else {
+//     rez = 'Неправильний пароль'};
+
+//     console.log(rez); 
+
+// 4 Завдання
+
+// let num = prompt('Будь яке число від 0 до 100')
+
+// let rez
+
+// if (num<0 || num>100)  { rez = 'Не належить'
+    
+// } else{rez = 'Належить'};
+
+// console.log(rez);
+
+// 5 Завдання
+
+// let  number1 = +prompt('будь-яке число')
+
+// let number2 = +prompt('будь-яке число')
+
+// let rez1 = number1 + number2;
+// let rez2 = number1 - number2;
+// let rez3 = number1 * number2;
+
+// if (number1<0 && number2<0){rez4}else{
+//     number1>0 && number2>0
+// }
+    
+
+// console.log(rez1);
+// console.log(rez2);
+// console.log(rez3);
+// console.log(rez4);
+
+
+// let  number1 = +prompt('будь-яке число');
+// let operator = prompt('Enter operator');
+// let number2 = +prompt('будь-яке число');
+// switch (operator) {
+//     case '+': console.log( ${number1} ${operator} ${number2} = ${number1 + number2});break
+//     case '-': console.log( ${number1} ${operator} ${number2} = ${number1 - number2});break
+//     case '*': console.log( ${number1} ${operator} ${number2} = ${number1 * number2});break
+//     case '/': {
+//         if (number2 == 0){console.log('ERROR');
+//         }else {console.log(`${number1}`);}
+//     }
+    
+        
+
+//     default: console.log(`incorect operator`);
+//         break;
+// }
+
+
+// 6 mision
+
+// let num1 = +prompt('Введіть рік народження');
+
+// console.log('вам ' + num1);
+
+// const num2 = 2023;
+
+// let rez;
+
+// if(num1>num2 || num1<1900 || isNaN(num1)) {console.log(Error);}
+// else{console.log(rez = num2 - num1);}
+
+// console.log('зараз вам ' + rez);
+
+// 7 mision
+
+let num1 = +prompt('Щомісячний дохід')
+
+let num2 = +prompt('витрати на комунальні послуги')
+
+
+
+
+
+
+
+
+
+
+
